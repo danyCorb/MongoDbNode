903e86c1415bca6fe0aee766ca20857c8664fc3e
BRANGERIEAU Thibaud <contact@brangerieau-thibaud.fr>
2019-01-21 09:28:32 +0100

    Ajout du nombre de cafée total

--- index/IndexRouter.js
+++ index/IndexRouter.js
@@ -219,10 +219,10 @@ router.get('/rqt8/:id', function(req, res) {
 router.get('/rqt9/:id', function(req, res) {
   Fac.aggregate([
   {$lookup:{from: 'individus', localField: 'profs', foreignField:'_id', as:'profs'}},
-  {$project:{matieres_enseignees:'$profs.matiere'}}, 
-  {$match:{_id:ObjectId(req.query.id)}}, 
-  {$unwind:'$matieres_enseignees'}, 
-  {$group:{_id: '$matieres_enseignees'}}, 
+  {$project:{matieres_enseignees:'$profs.matiere'}},
+  {$match:{_id:ObjectId(req.query.id)}},
+  {$unwind:'$matieres_enseignees'},
+  {$group:{_id: '$matieres_enseignees'}},
   {$group:{_id: 0, mats:{$push:'$_id'}}},
   {$project:{matieres_enseignees:'$mats', _id:0}}], function (err, result) {
           if (err) {
@@ -232,6 +232,20 @@ router.get('/rqt9/:id', function(req, res) {
           }
       }
   )
+})
+
+router.get('/rqt10', function(req, res){
+    Fac.aggregate([
+        {$lookup:{from: 'individus', localField: 'eleves', foreignField: '_id', as:'eleves'}},
+        {$lookup:{from: 'individus', localField: 'profs', foreignField: '_id', as:'profs'}},
+        {$project:{allSUm :{$sum: [{$sum: '$eleves.nb_cafe'}, {$sum: '$profs.nb_cafe'}]}}}], function (err, result) {
+            if (err) {
+                res.status(500).send("request error "+err);
+            } else {
+                res.status(200).send(result);
+            }
+        }
+    )
 })
 
 
